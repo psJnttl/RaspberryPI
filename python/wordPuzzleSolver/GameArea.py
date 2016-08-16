82b6c1dbdb6376e8266a888dec65ea3a613dc1b8
Pasi Junttila <pasi.junttila@live.com>
2016-08-16 15:15:10 +0300

    Add files via upload

---
+++ python/wordPuzzleSolver/GameArea.py
@@ -0,0 +1,152 @@
+﻿from SymbolScore import *
+from Vocabulary import *
+from FoundWordsList import *
+from GaStack import *
+from Symbol import *
+from Word import *
+
+import platform
+if platform.system() == 'Linux':
+    srvResultPath = './results/worDaDe_findings.txt'
+else: 
+    srvResultPath = '.\\results\\worDaDe_findings.txt'
+
+
+X_SIZE = 4
+Y_SIZE = 4
+MAX_WORD_LENGTH = X_SIZE * Y_SIZE
+
+class GameArea:
+    def __init__(self, gameAreaAsList=[]):
+        print 'now instantiating GameArea'
+        self.symbolScore = SymbolScore()
+        self.theVocabulary = Vocabulary()
+        print 'Vocabulary size: ', self.theVocabulary.NumberWords()
+        self.gameArea = []
+        self.tmpGameArea = []
+        if len(gameAreaAsList) != 16:
+            print 'Game Area size is not correct!'
+        else: 
+            self.InsertSymbols(gameAreaAsList)
+        self.skippedWords = 0
+
+    def InsertSymbols(self, gameArea):
+        self.gameArea = []; k = 0
+        for i in range(Y_SIZE):
+            tmpListX = []
+            for j in range(X_SIZE):
+                newSymbol = Symbol(gameArea[k], k, self.symbolScore.GetScore(gameArea[k]))
+                tmpListX.append(newSymbol)
+                k += 1
+            self.gameArea.append(tmpListX)
+
+    def InsertSymbolAt(self, gameArea, symbol, gY, gX):
+        if gY < 0 or gX < 0 or gY >= Y_SIZE or gX >= X_SIZE:
+            return
+        line = gameArea[gY]
+        line[gX] = symbol 
+
+    def Solve(self):
+        self.wordList = FoundWordsList()
+        for i in range(Y_SIZE):
+            tmpX = self.gameArea[i]  # collect line
+            for j in range(X_SIZE):  # x-coords
+                if tmpX[j].EndsWith() == True: # ends-with digrams
+                    print 'skipping tile '
+                    continue  # can't start with this symbol
+                self.theVocabulary.UseSubVocabulary(tmpX[j])
+                self.ProcessVocabulary(tmpX[j], i, j)
+                if tmpX[j].SymbolType() == 'eitherOr':
+                    tmpX[j].ToggleEoState()  # either -> or
+                    self.theVocabulary.UseSubVocabulary(tmpX[j])
+                    self.theVocabulary.ResetFailedAt()
+                    self.ProcessVocabulary(tmpX[j], i, j)
+        self.PrintGameArea(self.gameArea)
+        self.wordList.saveFindingsToFileJSON(srvResultPath) # also prints via Sort()
+        print 'skipped ', self.theVocabulary.SkippedWords(), ' words'
+
+    def ProcessVocabulary(self, symbol, gY, gX):
+        peaCounter = 0; lastWord = ''; nextWord = ''
+        while True:
+            nextWord = self.theVocabulary.GetNextWord()
+            if nextWord == False:
+                break
+            self.theVocabulary.ResetFailedAt()
+            self.tmpGameArea = self.CopyGameArea(self.gameArea)
+            self.word = Word(nextWord)  # path through wilderness
+            self.gaStack = GaStack()    # GameArea stack for backtracking
+            self.SearchPath(nextWord, 0, gY, gX)
+            if True == self.word.IsComplete():
+                self.wordList.InsertWord(self.word)
+                self.theVocabulary.ResetFailedAt()
+        return
+
+    def CopyGameArea(self, gameArea): # a copy of gameArea is used
+        tmpGameArea = []
+        for i in range(Y_SIZE):
+            tmpXdst = []
+            tmpXsrc = gameArea[i]
+            for j in range(X_SIZE):
+                newSymbol = tmpXsrc[j].Copy()
+                tmpXdst.append(newSymbol)
+            tmpGameArea.append(tmpXdst)
+        return tmpGameArea
+
+    def PrintGameArea(self, gameArea): # DEBUG purposes only
+        for i in range(Y_SIZE):
+            line = []
+            tmpX = gameArea[i]
+            for j in range(X_SIZE):
+                line.append(tmpX[j].SymbolChars())
+            print line
+
+    def FetchGameAreaSymbol(self, gameArea, gY, gX):
+        line = gameArea[gY]
+        symbol = line[gX]
+        return symbol
+
+    def SearchPath(self, nextWord, wordIndex, gY, gX):
+        if gY >= Y_SIZE or gY < 0 or gX >= X_SIZE or gX < 0 or wordIndex >= MAX_WORD_LENGTH:
+            return
+        tileSymbol = self.FetchGameAreaSymbol(self.tmpGameArea, gY, gX)   # no easier way ?
+        status = tileSymbol.IsNextSymbol(nextWord, wordIndex)
+        if status == 0:   #  tile symbol not next in word
+            symbolLength = tileSymbol.SymbolLength()
+            if symbolLength > 1: # digram
+                 self.theVocabulary.SetSpecialTile(tileSymbol)
+            return
+        elif status > 0:
+            if len(nextWord) == (wordIndex + status):  # word compete
+                self.word.InsertSymbol(tileSymbol.Copy())
+                self.word.SetComplete()
+                return
+            else:  # word only partially found
+                nro = Symbol(str(wordIndex),0,0)
+                self.gaStack.Push(self.CopyGameArea(self.tmpGameArea))  # store for backtracking
+                self.InsertSymbolAt(self.tmpGameArea, nro, gY, gX)  # mark game area
+                wordIndex += status
+                self.word.InsertSymbol(tileSymbol.Copy())
+
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY, gX+1) # E
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY+1, gX+1 ) # SE
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY+1, gX) # S
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY+1, gX-1) # SW
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY, gX-1) # W
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY-1, gX-1) # NW
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY-1, gX) # N
+        if False == self.word.IsComplete():
+            self.SearchPath(nextWord, wordIndex, gY-1, gX+1) # NE
+
+        if False == self.word.IsComplete():
+            self.tmpGameArea = self.gaStack.Pop()
+            self.theVocabulary.FailsAt(self.word, nextWord, wordIndex)
+            self.word.RemoveLastSymbol()            
+
+        return
---
+++ python/wordPuzzleSolver/Symbol.py
@@ -0,0 +1,110 @@
+﻿class Symbol:
+    def __init__(self, sym='a', coords=0, points=2):
+        self.symbol = sym    # 'a', 'ed', 'in-', '-est', 'm/i'.
+        self.symbolType = self.DetermineSymbolType(sym)
+        if self.symbolType == 'eitherOr': self.eoState = 'either'
+        else: self.eoState = ''
+        self.gaIndex = coords # not x,y but index
+        self.points = points
+
+    def SymbolChars(self):
+        if self.symbolType == 'letter' or self.symbolType == 'digram' or self.symbolType == 'path':
+            return self.symbol
+        elif self.symbolType == '1stDigram' or self.symbolType == 'lastDigram':
+            return self.symbol.strip('-')
+        elif self.symbolType == 'eitherOr':
+            if self.eoState == 'either': return self.symbol[:self.symbol.find('/')]
+            elif self.eoState == 'or': return self.symbol[self.symbol.find('/') +1 :]
+
+    def SymbolLength(self):
+        if self.symbolType == 'letter' or self.symbolType == 'digram' or self.symbolType == 'path':
+            return len(self.symbol)
+        elif self.symbolType == '1stDigram' or self.symbolType == 'lastDigram':
+            return len( self.symbol.strip('-') )
+        elif self.symbolType == 'eitherOr':
+            return 1  # assuming there won't be 'as/be' etc. only 'm/i'
+
+    def EndsWith(self): # in case symbol is one of the word's ending with: -ed, -est
+        if self.symbolType == 'lastDigram':
+            return True
+        return False        
+
+    def Coordinates(self): 
+        return self.gaIndex
+
+    def Points(self):
+        return self.points
+
+    def IsNextSymbol(self, word, wordIndex):
+        result = 0
+        if self.symbolType == 'letter' or self.symbolType == 'digram' :
+            result = self.IsNextLetterOrDigram(word, wordIndex)
+        elif self.symbolType == '1stDigram':
+            result = self.IsNext1stDigram(word, wordIndex)
+        elif self.symbolType == 'lastDigram':
+            result = self.IsNextLastDigram(word, wordIndex)
+        elif self.symbolType == 'eitherOr':
+            result = self.IsNextEitherOr(word, wordIndex)
+        return result # 0:fail, 1...4 nbr letters match depending on symbol type
+
+    def IsNextLetterOrDigram(self, word, wordIndex): # fail: 0, OK: amount of bytes consumed from word
+        testWord = word[wordIndex:]
+        foundAt = testWord.find(self.symbol)  # one or many chars
+        if foundAt == 0: # yes, it's next in the word
+            return len(self.symbol)
+        else: return 0
+
+    def IsNext1stDigram(self, word, wordIndex):
+        if wordIndex != 0: # doesn't start with == fail
+            return 0
+        foundAt = word.find(self.symbol[:-1])  # drop tailing '-'
+        if foundAt == 0:
+            return len(self.symbol[:-1])
+        return 0
+    
+    def IsNextLastDigram(self, word, wordIndex):
+        if 0 == word[wordIndex:].find(self.symbol[1:]):
+            return len(self.symbol) -1  # take '-' into account
+        return 0
+
+    def IsNextEitherOr(self, word, wordIndex): # test both, every time
+        testWord = word[wordIndex:]
+        sym = self.symbol[:self.symbol.find('/')]
+        if 0 == testWord.find(sym):
+            return 1
+        sym = self.symbol[self.symbol.find('/')+1:]
+        if 0 == testWord.find(sym):
+            return 1
+        return 0
+
+    def ToggleEoState(self):
+        if self.eoState == 'either': self.eoState = 'or'
+        elif self.eoState == 'or': self.eoState = 'either'
+        else: self.eoState = ''
+
+    def Copy(self):  # references don't fly very far
+        newSymbol = Symbol()
+        newSymbol.symbol = self.symbol; newSymbol.symbolType = self.symbolType; newSymbol.eoState = self.eoState
+        newSymbol.gaIndex = self.gaIndex; newSymbol.points = self.points
+        return newSymbol
+
+    def DetermineSymbolType(self, sym): # letter, digram, 1stDigram, lastDigram, eitherOr, path
+        result = False
+        if sym.isdigit():
+            return 'path'
+        elif len(sym) == 1 and sym.isalpha() :
+            result = 'letter'
+        elif len(sym) > 1 and len(sym) < 5: 
+            if len(sym) == 3 and 1 == sym.find('/') :
+                if True == sym[0].isalpha() and True == sym[2].isalpha():
+                    result = 'eitherOr'
+            elif 0 == sym.find('-') and sym[1:].isalpha() :
+                result = 'lastDigram'
+            elif (len(sym) - 1) == sym.find('-') and sym[:-1].isalpha():
+                result = '1stDigram'
+            elif sym.isalpha():
+                result = 'digram'
+        return result
+
+    def SymbolType(self):
+        return self.symbolType
