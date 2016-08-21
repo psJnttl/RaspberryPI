15d770c60d7de493c426247fb0337860490844b3
Pasi Junttila <pasi.junttila@live.com>
2016-08-21 16:22:09 +0300

    Create port.h

---
+++ baremetal/PDC8544scroller/source/port.h
@@ -0,0 +1,9 @@
+/*
+Raspberry Pi version 2.
+*/
+#define RPI_PERIPHERAL_BASE 0x3F000000
+
+typedef unsigned char   byte;  //   8bit
+typedef unsigned short  hword;  //  16bit
+typedef unsigned int   word;  //  32bit
+typedef unsigned long long int dword; //  64bit
